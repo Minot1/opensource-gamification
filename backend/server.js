755edfe62963ef5f1ebaf47bddaa5305b70b77ab
Taner Dincer <dincer.taner123@gmail.com>
2024-09-28 20:14:01 +0100

    DB configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Success");
